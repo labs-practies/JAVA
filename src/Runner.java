111affdcc08d13e2ab72bdad0f174aad5422098a
Ivan Vlazniuk <45821628+Saipience@users.noreply.github.com>
2019-03-15 23:35:01 +0300

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,37 +1,37 @@
 import by.gsu.asoilab.BusinessTrip;
 import by.gsu.asoilab.Converter;
+    public class Runner {
 
-public class Runner {
+        public static void main(String[] args) {
 
-	public static void main(String[] args) {
+            BusinessTrip[] trips = new BusinessTrip[] {
+                    new BusinessTrip("Jon", 110, 210),
+                    new BusinessTrip(null,0,0),
+                    new BusinessTrip("Josh", 231, 85),
+                    new BusinessTrip("Brad", 233, 222),
+                    new BusinessTrip("Ilon", 208, 37),
+                    new BusinessTrip("Clark", 99, 45),
+                    new BusinessTrip() };
 
-		BusinessTrip[] trips = new BusinessTrip[] {
-				new BusinessTrip("Jon", 110, 210), null,
-				new BusinessTrip("Josh", 231, 85),
-				new BusinessTrip("Brad", 233, 222),
-				new BusinessTrip("Ilon", 208, 37),
-				new BusinessTrip("Clark", 99, 45),
-				new BusinessTrip() };
+            for (BusinessTrip trip : trips) {
+                if (trip != null) {
+                    trip.show();
+                }
 
-		for (BusinessTrip trip : trips) {
-			if (trip != null) {
-				trip.show();
-			}
+            }
 
-		}
+            trips[trips.length - 1].setTransport(30);
+            System.out.println("Duration = " + (trips[0].getDays() + trips[1].getDays()));
 
-		trips[trips.length - 1].setTransport(30);
-		System.out.println("Duration = " + (trips[0].getDays() + trips[1].getDays()));
-		
-		for (BusinessTrip businessTrip : trips) {
-			System.out.println(businessTrip);
-		}
-		
-		int totalSum = 0;
-		for(BusinessTrip trip : trips){
-			totalSum += trip.getTotal();
-		}
-		System.out.println(totalSum);
+            for (BusinessTrip businessTrip : trips) {
+                System.out.println(businessTrip);
+            }
 
-	}
-}
+            int totalSum = 0;
+            for(BusinessTrip trip : trips){
+                totalSum += trip.getTotal();
+            }
+            System.out.println(totalSum);
+
+        }
+    }
